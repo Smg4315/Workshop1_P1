a8df38eac71364b057d636d89326f88b4725d20d
Saimon <smazog@eafit.edu.co>
2025-07-31 11:49:23 -0500

    advancing throug adding movies

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def home(request):
     #return HttpResponse('<H1> Welcome to the Movie Reviews Home Page! </H1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html',  {'name':'Simón Mazo Gómez'})
+    #return render(request, 'home.html',  {'name':'Simón Mazo Gómez'})
+    searchtearm= request.GET.get('search') 
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchtearm, 'movies': movies })
+
 
 def about(request):
     #return HttpResponse('<H1> Welcome to about page </H1>')
